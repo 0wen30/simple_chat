8047298ab0db2a1b7ad361407ba8798d838e22c5
jose angel espinosa <owen9330@gmail.com>
2022-06-22 11:31:26 -0500

    simple_chat

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const { join } = require('path');
+
 const port = process.env.PORT || 5000;
 
 app.use('/', express.static('public'));
+
+app.get('/', (req,res)=>{
+  res.sendFile(join(__dirname,"public","index.html"))
+});
 
 io.on('connection', (socket) => {
 
